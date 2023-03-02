94735f747e4cfe0587042e9854fe0b5f6b4c68e9
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-03-02 15:17:19 -0800

    add testing and separate App component, and ignore bundle.js files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-const App = () => {
-  return (
-    <div>Hello World!</div>
-  )
-};
+import App from './components/App.jsx';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App/>);
